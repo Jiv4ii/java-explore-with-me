c011c0de5a7f5c9e32d2533f6bc1f9757dd4fc23
Jiv4ii <usaaalee@gmail.com>
2024-02-01 16:54:57 +0500

    Диплом 3.0 испрвлеывыв

--- ewm-service/src/main/java/ru/practicum/project/exceptions/CommentNotFoundException.java
+++ ewm-service/src/main/java/ru/practicum/project/exceptions/CommentNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.practicum.project.exceptions;
 
-public class CommentNotFoundException extends RuntimeException{
+public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(String message) {
         super(message);
     }
